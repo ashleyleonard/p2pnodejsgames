81781d978045ae210bff16ed92a15a2657b927fd
ashleyleonard <ashley.leonard777@gmail.com>
2013-06-13 23:36:27 +0200

    update port al

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen(80);
 
 var logger  = io.log;
 //var sleep = require('sleep');
